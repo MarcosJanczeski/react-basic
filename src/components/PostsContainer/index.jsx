8bd199b7dcbfc36735f6dde13cd1cfda4c8684c7
MARCOS JANCZESKI <marcosjanczeski@gmail.com>
2021-06-16 20:42:21 -0400

    implementando useeffect

--- src/components/PostsContainer/index.jsx
+++ src/components/PostsContainer/index.jsx
@@ -1,79 +1,60 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import { PostCard } from '../PostCard'
 import loadPosts from './loadPosts'
 
-export default class PostsContainer extends Component {
-  state = {
-    posts: [],
-    start: 0,
-    step: 8,
-    filter: '',
-    show: []
+const PostsContainer = () => {
+  const [posts, setPosts] = useState([])
+  const [start, setStart] = useState(0)
+  const [step] = useState(8)
+  const [filter, setFilter] = useState('')
+  const [show, setShow] = useState([])
+
+  useEffect(() => mountPosts(), [])
+
+  const mountPosts = async () => {
+    const loadedPosts = await loadPosts()
+    setPosts(loadedPosts)
+    setFilter('')
+    setShow(loadedPosts.slice(start, step))
   }
 
-  componentDidMount() {
-    this.mountPosts()
+  const nextPosts = () => {
+    setStart(start + step)
+    setShow(posts.slice(start, start + step))
   }
 
-  mountPosts = async () => {
-    const posts = await loadPosts()
-    this.setState(state => ({
-      ...state,
-      posts,
-      filter: '',
-      show: posts.slice(state.start, state.step)
-    }))
+
+  const filterPosts = e => {
+    const filterValue = e.target.value
+    if (!!filterValue) {
+      setFilter(filterValue)
+      setShow(posts.filter(post => (
+        post.title.toLowerCase().includes(filterValue.toLowerCase()) ||
+        post.body.toLowerCase().includes(filterValue.toLowerCase())
+      )))
+    } else mountPosts()
   }
 
-  nextPosts = () => {
-    this.setState(state => {
-      let { posts, start, step } = state
-      start += step
-      return {
-        ...state,
-        start,
-        show: posts.slice(start, start + step)
-      }
-    })
-  }
-
-  filterPosts = e => {
-    const filter = e.target.value
-    !!filter ? (
-      this.setState(state => ({
-        ...state,
-        filter,
-        show: state.posts.filter(post => (
-          post.title.toLowerCase().includes(filter.toLowerCase()) ||
-          post.body.toLowerCase().includes(filter.toLowerCase())
-        ))
-      }))
-    ) : (
-      this.mountPosts()
-    )
-  }
-
-  render() {
-    const { show, start, step, filter } = this.state
-    return (
-      <>
-        <input
-          type="search"
-          value={filter}
-          onChange={this.filterPosts}
-        />
-        <div className="posts">
-          {show.map(post => (
-            <PostCard key={post.id} post={post} />
-          ))}
-        </div>
-        <button
-          onClick={this.nextPosts}
-          disabled={start + step >= this.state.posts.length || !!filter}
-        >
-          Next
-        </button>
-      </>
-    )
-  }
+  return (
+    <>
+      <input
+        type="search"
+        value={filter}
+        onChange={filterPosts}
+      />
+      <div className="posts">
+        {show.map(post => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+      <button
+        onClick={nextPosts}
+        disabled={start + step >= posts.length || !!filter}
+      >
+        Next
+      </button>
+    </>
+  )
 }
+
+export default PostsContainer
